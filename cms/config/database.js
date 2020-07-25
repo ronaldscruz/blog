c77cc21d06435b123db28e46c3f47955fcfc7318
ronaldsfcruz <ronald.cruz@afya.com.br>
2020-07-25 17:27:17 -0300

    CMS set up

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,15 +1,22 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
+
+module.exports = () => ({
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: process.env.DATABASE_HOST || "127.0.0.1",
+        port: process.env.DATABASE_PORT || 3306,
+        database: process.env.DATABASE_NAME || "db_rscruz_cms",
+        username: process.env.DATABASE_USERNAME || "root",
+        password: process.env.DATABASE_PASSWORD || "example",
+        ssl: process.env.DATABASE_SSL === "true",
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
